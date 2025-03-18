6c4decd9261383d98a6edf67cf20e1fcc10a8f50
donipit99 <donipit99@gmail.com>
2025-03-18 16:16:50 +0300

    Добавил

---
+++ pkg/repository/repository.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	todo "github.com/donipit99/todo-app"
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func (r Repository) CreateUser(user todo.User) (int, error) {
+	panic("unimplemented")
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
+
+}
